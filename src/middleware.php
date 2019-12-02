cbececbf838e68ed8a5a5ea28dfa2ff220b57285
Sille Van Landschoot <info@sillevl.be>
2019-12-02 22:14:26 +0100

    allow cors for frontend

--- src/middleware.php
+++ src/middleware.php
@@ -9,6 +9,7 @@ use Slim\App;
 return function (App $app) {
     $app->add(function (Request $request, RequestHandler $handler) {
       $response = $handler->handle($request);
-      return $response->withHeader('Content-Type', 'application/json');
+      return $response->withHeader('Content-Type', 'application/json')
+                      ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
     });
 };
